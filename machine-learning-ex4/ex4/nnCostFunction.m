e6a9f1f1907569a7b7eaf29e71212a6f41c621c5
Tan Van Ho <tan.hv@cloud9-solutions.com>
2017-09-18 11:12:15 +0700

    Update ex4

--- machine-learning-ex4/ex4/nnCostFunction.m
+++ machine-learning-ex4/ex4/nnCostFunction.m
@@ -63,14 +63,34 @@ Theta2_grad = zeros(size(Theta2));
 %
 
 
+y_matrix = eye(num_labels)(y,:);
+
+a1 = [ones(m, 1) X];  % Add bias column
+z2 = a1 * Theta1';
+a2 = sigmoid(z2);
+a2 = [ones(m, 1) a2];
+z3 = a2 * Theta2';
+a3 = sigmoid(z3);
+
+J_unreg = -1/m * sum(sum(y_matrix .* log(a3) + (1 - y_matrix) .* log(1 - a3)));
+
+J = J_unreg + lambda/(2*m) * ((sum(sum(power(Theta1(:,2:end),2)))) + (sum(sum(power(Theta2(:,2:end),2))))) ;
 
 
+% Back propagation
 
+d3 = a3 - y_matrix;
+d2 = d3 * Theta2(:,2:end) .* sigmoidGradient(z2);
 
+Delta1 = d2' * a1;
+Delta2 = d3' * a2;
 
+Theta1_grad = 1/m * Delta1;
+Theta2_grad = 1/m * Delta2;
 
-
-
+% Regularization
+Theta1_grad = Theta1_grad + (lambda/m) * [zeros(size(Theta1, 1), 1) Theta1(:,2:end)];
+Theta2_grad = Theta2_grad + (lambda/m) * [zeros(size(Theta2, 1), 1) Theta2(:,2:end)];
 
 
 
